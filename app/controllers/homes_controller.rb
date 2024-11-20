e72ce79baa9195493193c84dea43dc74be1d3836
Zack <168221270+Zval321@users.noreply.github.com>
2024-11-20 14:47:49 -0600

    added api create action to google servers to dynamically search cities

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,6 @@
 class HomesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+  
   require 'httparty'
 
   def index
@@ -12,5 +14,28 @@ class HomesController < ApplicationController
     data = response.parsed_response
 
     render json: { message: 'HomePage', api_response: data }
+  end
+
+  def create 
+    city = params[:city]
+
+    if city.blank?
+      render json: { error: 'City name is required'}, status: :bad_request
+      return 
+    end 
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/place/textsearch/json", {
+      query: {
+        query: "city #{city}",
+        key: ENV['GOOGLE_PLACES_API_KEY']
+      }
+    })
+
+    data = response.parsed_response
+
+    if response.code == 200
+      render json: { message: "Success", api_response: data }
+    else
+      render json: { error: data['error_message'] || 'An error occurred' }, status: :unprocessable_entity
+    end
   end
 end
