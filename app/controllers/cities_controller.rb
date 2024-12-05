8397752cd3351349ccd0620adc11d2925c1de403
Zack <168221270+Zval321@users.noreply.github.com>
2024-12-04 21:36:07 -0600

    updated cities show action

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -8,61 +8,50 @@ class CitiesController < ApplicationController
     render :index
   end 
 
-  def show
-    city_name = params[:id]  
-    @city = City.find_by(name: city_name)
-
-    unless @city
-      response = HTTParty.get("https://maps.googleapis.com/maps/api/place/textsearch/json", {
-        query: {
-          query: "city #{city_name}",
-          key: ENV['GOOGLE_PLACES_API_KEY']
-        }
-      })
-      data = response.parsed_response
-
-      if response.code == 200 && data['results'].any?
-        new_city_data = data['results'].first
-        @city = City.create(
-          name: new_city_data['name'],
-          location: new_city_data['formatted_address'],
-          user_id: current_user.id
-        )
-      else
-        render json: { error: 'Failed to fetch data from the API' }, status: :unprocessable_entity
-        return
-      end
-    end
-
-    @comments = @city.comments
-    render json: { city: @city, comments: @comments }
+  def show 
+    @city = City.find_by(id: params[:id])
+    render :show
   end
 
   def create
     city_name = params[:city_name]
-
+  
     response = HTTParty.get("https://maps.googleapis.com/maps/api/place/textsearch/json", {
       query: {
-        query: "city #{city_name}",
+        query: "city #{city_name}, USA",  # Add the country for more precision
         key: ENV['GOOGLE_PLACES_API_KEY']
       }
     })
-
+  
     data = response.parsed_response
-
+  
     if response.code == 200 && data['results'].any?
       new_city_data = data['results'].first
-
+  
       @city = City.find_or_create_by(
         name: new_city_data['name'],
         location: new_city_data['formatted_address'],
         user_id: current_user.id
       )
-
+  
       render json: @city, status: :created
     else
       render json: { error: 'Failed to fetch data from the API' }, status: :unprocessable_entity
     end
   end
+
+  def destroy 
+    @city = City.find_by(id: params[:id])
+    if @city.nil? 
+      return render json: { error: 'City not found'}, status: :not_found
+    end
+    @city.comments.destroy_all
+
+    if @city.destroy
+      render json: {message: 'The city has been deleted'}, status: :ok
+    else 
+      render json: {error: 'the city could not be deleted'}, status: :bad_request
+    end
+  end 
   
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,12 @@ class CommentsController < ApplicationController
         user_id: params[:user_id],
         city_id: params[:city_id]
       )
-      @comment.save
+      if @comment.save
+        render json: @comment, status: :created
+      else
+        Rails.logger.error "Comment could not be saved: #{@comment.errors.full_messages.join(', ')}"
+        render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+      end
   end 
 
 end
