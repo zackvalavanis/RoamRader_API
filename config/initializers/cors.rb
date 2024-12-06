f0b6b4b05a6fb3572428320afe7ce9ce07819b97
Zack <168221270+Zval321@users.noreply.github.com>
2024-12-05 19:34:33 -0600

    updates

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:5173' # Allow frontend domains (you can add multiple origins)
+    origins '*' # Allow all origins
 
-    resource '*',
+    resource '*', 
       headers: :any, # Allow all headers
       methods: [:get, :post, :options, :put, :patch], # Allowed HTTP methods
-      credentials: true # Enable cookies, authorization headers, or TLS client certificates
+      credentials: false # Enable cookies, authorization headers, or TLS client certificates
   end
 end
