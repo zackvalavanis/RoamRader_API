63973b81ed4f1af39356eadb1f7c27403a0f7675
Zack <168221270+Zval321@users.noreply.github.com>
2024-12-07 15:30:59 -0600

    admin functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,15 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       jwt = JWT.encode(
         {
-          user_id: user.id, # the data to encode
+          user_id: user.id,         # the data to encode
+          email: user.email,        # include email if needed in the JWT
+          is_admin: user.is_admin,  # include the is_admin field
           exp: 24.hours.from_now.to_i # the expiration time
         },
         Rails.application.credentials.fetch(:secret_key_base), # the secret key
         "HS256" # the encryption algorithm
       )
-      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+      render json: { jwt: jwt, user_id: user.id, email: user.email, is_admin: user.is_admin }
     else
       render json: {}, status: :unauthorized
     end
